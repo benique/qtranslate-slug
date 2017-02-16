d2544311efa49ee098eb30f93a3c99eb7e11c4ac
benique <b.martin@marketvision.ch>
2017-02-16 21:07:24 +0700

    Update class-qtranslate-slug.php
    There was an undefined index warning before. So I fixed it.

--- includes/class-qtranslate-slug.php
+++ includes/class-qtranslate-slug.php
@@ -990,6 +990,7 @@ class QtranslateSlug {
 			$cache_array = array($page);
 			update_post_caches($cache_array, 'page'); // caching query :)
 			wp_cache_delete('qts_page_request');
+			$query['name']=isset($query['name'])?$query['name']:NULL;
 			$query['qts_slug_name'] = $query['name'];
 			unset( $query['name'] );
 			$query['pagename'] = get_page_uri($page);
